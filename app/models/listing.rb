301dc69bc9a18abf1d51fef6d1b72954033172ce
Erik Theuer <erik.theuer@gmail.com>
2014-07-18 23:53:36 -0300

    changed URL for default image in listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "https://www.dropbox.com/s/vxo86coujq0k88n/missing.jpg",
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg",
 #	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/,
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml")
